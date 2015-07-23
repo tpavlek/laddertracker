65af5897b6c91fef537ff5b340f98af94ead0aea
Troy Pavlek <troy@tpavlek.me>
2015-07-23 14:56:14 -0600

    Added web view

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var autoprefix = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    return gulp.src([
+        'resources/css/fonts.css',
+        'resources/css/pure.min.css',
+        'resources/css/grids-responsive.min.css',
+        'resources/scss/style.scss'
+    ])
+        .pipe(sass({ style: "expanded" }))
+        .pipe(concat('all.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(minifyCSS({aggressiveMerging: false }))
+        .pipe(gulp.dest('./public/css/'))
+});
--- public/index.php
+++ public/index.php
@@ -1,16 +1,46 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 require '../bootstrap.php';
+
+$loader = new Twig_Loader_Filesystem('../resources/views');
+$twig = new Twig_Environment($loader);
 
 $router = new \League\Route\RouteCollection;
 
-$router->addRoute('GET', '/', function(\Symfony\Component\HttpFoundation\Request $request, \Symfony\Component\HttpFoundation\Response $response) {
+$controller = new \Depotwarehouse\LadderTracker\Client\Web\HomeController(
+    new \Depotwarehouse\LadderTracker\Database\User\UserRepository($capsule->getConnection(), new \Depotwarehouse\LadderTracker\Database\User\UserConstructor()),
+    $twig
+);
 
-    $response->setContent("Weow");
+$router->addRoute('GET', '/', function(\Symfony\Component\HttpFoundation\Request $request, \Symfony\Component\HttpFoundation\Response $response) use ($controller) {
+    $content = $controller->home();
+
+    $response->setContent($content);
+    return $response;
+});
+
+$router->addRoute('GET', '/about', function (Request $request, \Symfony\Component\HttpFoundation\Response $response) use ($controller) {
+    $content = $controller->about();
+
+    $response->setContent($content);
     return $response;
 });
 
 $dispatcher = $router->getDispatcher();
 
-$response = $dispatcher->dispatch('GET', '/');
-$response->send();
+$request = Request::createFromGlobals();
+
+try {
+    $response = $dispatcher->dispatch($request->getMethod(), $request->getPathInfo());
+    $response->send();
+} catch (\Exception $exception) {
+    $response = new \Symfony\Component\HttpFoundation\Response();
+    $response->setContent($exception->getMessage());
+    $response->send();
+}
+
+
+
+
