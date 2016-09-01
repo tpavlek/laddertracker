beca84719609a1d8220364cffa89cd4b00250c1a
Troy Pavlek <troy@depotwarehouse.net>
2016-09-01 16:35:53 -0600

    Connection is raw

--- src/Database/User/UserRepository.php
+++ src/Database/User/UserRepository.php
@@ -104,7 +104,7 @@ class UserRepository
         $result = $this->connection->table('laddertracker_events')
             ->where('aggregateId', '=', $userId)
             ->where('eventName', '=', RankChangedEvent::class)
-            ->select([ \DB::raw('max(timestamp) as timestamp') ])
+            ->select([ $this->connection->raw('max(timestamp) as timestamp') ])
             ->first();
 
         return new Carbon($result->timestamp);
