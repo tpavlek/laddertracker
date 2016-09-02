86a0cd61fec60518020f2c5faf753193a6e5d637
Troy Pavlek <troy@tpavlek.me>
2016-09-02 16:23:04 -0600

    30

--- web/app/Http/Controllers/HomeController.php
+++ web/app/Http/Controllers/HomeController.php
@@ -24,8 +24,8 @@ class HomeController extends Controller
     public function index()
     {
 
-        $naUsers = $this->userRepository->top(25, Region::america(), UserRepository::SORT_LADDER_POINTS, 'DESC');
-        $euUsers = $this->userRepository->top(25, Region::europe(), UserRepository::SORT_LADDER_POINTS, 'DESC');
+        $naUsers = $this->userRepository->top(30, Region::america(), UserRepository::SORT_LADDER_POINTS, 'DESC');
+        $euUsers = $this->userRepository->top(30, Region::europe(), UserRepository::SORT_LADDER_POINTS, 'DESC');
 
         return view('index')
             ->with('naUsers', $naUsers)
