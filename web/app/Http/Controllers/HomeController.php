e62755d86faf5ed4b0e4ddc5324c9c7d6fdd2adb
Ravi Pareek <feardragon64@gmail.com>
2016-12-08 19:14:03 -0800

    Shows top 50 instead of top 30 on home page

--- web/app/Http/Controllers/HomeController.php
+++ web/app/Http/Controllers/HomeController.php
@@ -24,8 +24,8 @@ class HomeController extends Controller
     public function index()
     {
 
-        $naUsers = $this->userRepository->top(30, Region::america(), UserRepository::SORT_LADDER_POINTS, 'DESC');
-        $euUsers = $this->userRepository->top(30, Region::europe(), UserRepository::SORT_LADDER_POINTS, 'DESC');
+        $naUsers = $this->userRepository->top(50, Region::america(), UserRepository::SORT_LADDER_POINTS, 'DESC');
+        $euUsers = $this->userRepository->top(50, Region::europe(), UserRepository::SORT_LADDER_POINTS, 'DESC');
         $naLockDate = $this->monthRepository->getNextLockDate(Region::america());
         $euLockDate = $this->monthRepository->getNextLockDate(Region::europe());
 
