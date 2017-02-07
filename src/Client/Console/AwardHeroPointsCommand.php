34f0aa2b925f568edd8eb5eb9e3e581fc3b13f44
Ravi Pareek <feardragon64@gmail.com>
2017-02-06 23:22:00 -0800

    Fixes award console method

--- src/Client/Console/AwardHeroPointsCommand.php
+++ src/Client/Console/AwardHeroPointsCommand.php
@@ -3,8 +3,10 @@
 namespace Depotwarehouse\LadderTracker\Client\Console;
 
 use Carbon\Carbon;
+use Depotwarehouse\BattleNetSC2Api\Region;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AwardHeroPointsCommand extends Command
@@ -21,15 +23,18 @@ class AwardHeroPointsCommand extends Command
     public function configure()
     {
         $this->setName('laddertracker:award');
+        $this->addArgument('region', InputArgument::REQUIRED, 'na or eu');
         $this->setDescription('Award hero points to all users that deserve them');
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $date = Carbon::now()->toDateTimeString();
-
-        $this->internalCommand->run();
-        $output->writeln("[$date] Awarded hero points to all users!");
+        $region = $input->getArgument('region') == 'na' ?
+            \Depotwarehouse\LadderTracker\ValueObjects\Region::america() :
+            \Depotwarehouse\LadderTracker\ValueObjects\Region::europe();
+        $this->internalCommand->run($region);
+        $output->writeln("[$date] Awarded hero points to all users in region " . $region);
     }
 
 }
